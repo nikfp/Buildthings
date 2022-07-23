9a1b10ecaa589f1567bca4323fa5bef8fb16e8d9
nikfp <nikprotsman@gmail.com>
2022-07-23 13:41:44 -0600

    added dialog form and used for customer edit

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import 'agnostic-svelte/css/common.min.css';
+  import { Dialog } from "agnostic-svelte";
+
+  export let title: string;
+
+  let dialogInstance: Dialog;
+  const assignDialogInstance = (ev: any) => {
+    dialogInstance = ev.detail.instance;
+  };
+
+  export const openDialog = () => {
+    if (dialogInstance) {
+      console.log(dialogInstance);
+      dialogInstance.show();
+    }
+  };
+
+  export const closeDialog = () => {
+    if(dialogInstance) {
+      dialogInstance.hide();
+    }
+  }
+</script>
+
+<Dialog {title} id="a11y-dialog" dialogRoot="#dialog-root" closeButtonLabel="My close button label" closeButtonPosition="last" titleId="uniqueTitleId" role="dialog" classNames={{ title: 'h4 mbe18 flex justify-center' }} isAnimationFadeIn isAnimationSlideUp on:instance={assignDialogInstance} >
+  <slot />
+</Dialog>
--- src/routes/customers/[id].svelte
+++ src/routes/customers/[id].svelte
@@ -10,50 +10,62 @@
 <script lang="ts">
   import {query, graphql, mutation, type Customer, GQL_UpdateCustomer} from "$houdini"
   import type { LoadEvent } from '@sveltejs/kit';
-import FormBuilder from "../../lib/components/FormBuilder.svelte";
-import {updateCustomerSchema} from '$modules/Customer/validators'
+  import FormBuilder from "../../lib/components/FormBuilder.svelte";
+  import {updateCustomerSchema} from '$modules/Customer/validators'
+  import Edit from "../../lib/components/icons/Edit.svelte";
+  import Dialog from '../../lib/components/Dialog.svelte';
 
+  let dialog: Dialog; 
   const {data} = query<Customer>(graphql`
-  query Customer($id: ID!) {
-  getCustomerById(id: $id) {
-    id
-    name
-  }
-}
-  `)
+    query Customer($id: ID!) {
+      getCustomerById(id: $id) {
+        id
+        name
+      }
+    } 
+  `);
 
   const {} = mutation(graphql`
-  mutation UpdateCustomer($input: UpdateCustomerInput!) {
-  UpdateCustomer(input: $input) {
-    id
-    name
-  }
-}
-  `)
+    mutation UpdateCustomer($input: UpdateCustomerInput!) {
+      UpdateCustomer(input: $input) {
+        id
+        name
+      } 
+    }
+  `);
+
+
 </script>
 
 {#if $data}
-<p>{$data.getCustomerById.name}</p>
-<br />
-<br />
-<br />
-<FormBuilder config={{
+<h1>{$data.getCustomerById.name}<span on:click={() => dialog.openDialog()} class="edit-button"><Edit/></span></h1>
+<Dialog bind:this={dialog} title="Edit Customer">
+  <FormBuilder config={{
     mutator: GQL_UpdateCustomer,
     validator: updateCustomerSchema,
+    onSuccessfulSubmit: () => dialog.closeDialog(),
     fieldConfig: {
-    id: {
-      fieldType: "STRING",
-      hidden: true,
-      defaultValue: $data.getCustomerById.id
-    },
-    name: {
-      fieldType: "STRING",
-      label: "Customer Name",
-      defaultValue: $data.getCustomerById.name
+      id: {
+        fieldType: "STRING",
+        hidden: true,
+        defaultValue: $data.getCustomerById.id
+      },
+      name: {
+        fieldType: "STRING",
+        label: "Customer Name",
+        defaultValue: $data.getCustomerById.name
+      }
     }
-  }
-  
-}} />
+    
+  }} />
+</Dialog>
 {:else}
 <p>Loading....</p>
 {/if}
+
+<style>
+  .edit-button {
+    padding: 0 .75rem;
+    height: 3rem;
+  }
+</style>
